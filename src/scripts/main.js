f62ab9522fe8e75455bdfebe11137ff2072a0b54
Eric Leite <ericxleite@gmail.com>
2018-07-23 23:52:42 -0700

    Removed flash of unstyled content

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,10 @@
 
 const Homepage = require('homepage');
 
+// Remove static class from body.
+document.body.classList.remove('static');
+
+// Add onLoad event listener.
 document.addEventListener("DOMContentLoaded", () => {
   Homepage.initialize();
 });
